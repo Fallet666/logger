0fd37bfe207f074e176a1e02f87cc709807ffab2
Alex Korotkov <115859651+Fallet666@users.noreply.github.com>
2024-07-31 18:59:33 +0300

    corrections (log message, log level etc)

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,85 +2,95 @@
 #include <gtest/gtest.h>
 #include <sstream>
 #include <fstream>
-#include <filesystem>
-
+#include <regex>
+#include <unordered_set>
 
 class LoggerTest : public ::testing::Test {
 protected:
     std::ostringstream oss;
     Logger::Logger logger{"TestLogger", oss};
 
-    void SetUp() override {
-        oss.str("");
-        oss.clear();
+    static std::string buildExpectedLogString(Logger::LogLevel level, const std::string& loggerName, const std::string& message) {
+        std::string level_str = Logger::toString(level);
+        return level_str + ": \\d{2}:\\d{2}:\\d{2} \\[" + loggerName + "\\]: " + message + "\n?";
+    }
+
+    void checkLog(const std::string& expectedLog) {
+        std::regex logRegex(expectedLog);
+        bool match = std::regex_match(oss.str(), logRegex);
+        if (!match) {
+            std::cout << "Log does not match. Log: \"" << oss.str() << "\" Expected: \"" << expectedLog << "\"" << std::endl;
+        }
+        EXPECT_TRUE(match);
     }
 };
 
 TEST_F(LoggerTest, DebugMessage) {
     logger.logMessage(Logger::DEBUG, "Debug message");
-    EXPECT_EQ(oss.str(), "DEBUG: " + fmt::format("{:%H:%M:%S}", fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()))) + " [TestLogger]: Debug message\n");
+    checkLog(buildExpectedLogString(Logger::DEBUG, "TestLogger", "Debug message"));
 }
 
 TEST_F(LoggerTest, InfoMessage) {
     logger.logMessage(Logger::INFO, "Info message");
-    EXPECT_EQ(oss.str(), "INFO: " + fmt::format("{:%H:%M:%S}", fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()))) + " [TestLogger]: Info message\n");
+    checkLog(buildExpectedLogString(Logger::INFO, "TestLogger", "Info message"));
 }
 
 TEST_F(LoggerTest, WarningMessage) {
     logger.logMessage(Logger::WARN, "Warning message");
-    EXPECT_EQ(oss.str(), "WARN: " + fmt::format("{:%H:%M:%S}", fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()))) + " [TestLogger]: Warning message\n");
+    checkLog(buildExpectedLogString(Logger::WARN, "TestLogger", "Warning message"));
 }
 
 TEST_F(LoggerTest, ErrorMessage) {
     logger.logMessage(Logger::ERROR, "Error message");
-    EXPECT_EQ(oss.str(), "ERROR: " + fmt::format("{:%H:%M:%S}", fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()))) + " [TestLogger]: Error message\n");
+    checkLog(buildExpectedLogString(Logger::ERROR, "TestLogger", "Error message"));
 }
 
 TEST_F(LoggerTest, MultiThreadedLogging) {
-    auto log_function = [this](Logger::LogLevel level, const std::string& message) {
-        logger.logMessage(level, message);
-    };
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 10; ++i) {
+        threads.emplace_back([this, i] {
+            logger.logMessage(Logger::INFO, "Message " + std::to_string(i));
+        });
+    }
+    for (auto &t : threads) {
+        t.join();
+    }
 
-    std::thread t1(log_function, Logger::DEBUG, "Debug message from thread 1");
-    std::thread t2(log_function, Logger::INFO, "Info message from thread 2");
-    std::thread t3(log_function, Logger::WARN, "Warning message from thread 3");
-    std::thread t4(log_function, Logger::ERROR, "Error message from thread 4");
+    std::istringstream logStream(oss.str());
+    std::string logEntry;
+    int messageCount = 0;
 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
+    while (std::getline(logStream, logEntry)) {
+        std::regex logRegex("INFO: \\d{2}:\\d{2}:\\d{2} \\[TestLogger\\]: Message \\d");
+        bool match = std::regex_match(logEntry, logRegex);
+        EXPECT_TRUE(match);
+        if (match) {
+            messageCount++;
+        }
+    }
 
-    // Проверка того, что все сообщения были залогированы
-    EXPECT_NE(oss.str().find("DEBUG: "), std::string::npos);
-    EXPECT_NE(oss.str().find("INFO: "), std::string::npos);
-    EXPECT_NE(oss.str().find("WARN: "), std::string::npos);
-    EXPECT_NE(oss.str().find("ERROR: "), std::string::npos);
+    EXPECT_EQ(messageCount, 10);
 }
 
-TEST_F(LoggerTest, FileLogging) {
-    std::string log_filename = "test_log.txt";
-    {
-        std::ofstream file(log_filename);
-        Logger::Logger file_logger("FileLogger", file);
-        file_logger.logMessage(Logger::DEBUG, "Debug message");
-        file_logger.logMessage(Logger::INFO, "Info message");
-        file_logger.logMessage(Logger::WARN, "Warning message");
-        file_logger.logMessage(Logger::ERROR, "Error message");
+TEST_F(LoggerTest, LogToFile) {
+    std::ofstream file("test_log.txt");
+    Logger::Logger fileLogger("FileLogger", file);
+
+    fileLogger.logMessage(Logger::DEBUG, "File log message");
+
+    file.close();
+
+    std::ifstream fileRead("test_log.txt");
+    std::stringstream buffer;
+    buffer << fileRead.rdbuf();
+    fileRead.close();
+
+    std::regex logRegex(buildExpectedLogString(Logger::DEBUG, "FileLogger", "File log message"));
+    bool match = std::regex_match(buffer.str(), logRegex);
+    if (!match) {
+        std::cout << "Log does not match. Log: \"" << buffer.str() << "\" Expected: \"" << buildExpectedLogString(Logger::DEBUG, "FileLogger", "File log message") << "\"" << std::endl;
     }
+    EXPECT_TRUE(match);
 
-    std::ifstream file(log_filename);
-    std::string line;
-    std::vector<std::string> lines;
-    while (std::getline(file, line)) {
-        lines.push_back(line);
-    }
-
-    ASSERT_EQ(lines.size(), 4);
-    EXPECT_NE(lines[0].find("DEBUG: "), std::string::npos);
-    EXPECT_NE(lines[1].find("INFO: "), std::string::npos);
-    EXPECT_NE(lines[2].find("WARN: "), std::string::npos);
-    EXPECT_NE(lines[3].find("ERROR: "), std::string::npos);
-
-    std::filesystem::remove(log_filename);
+    std::remove("test_log.txt");
 }
