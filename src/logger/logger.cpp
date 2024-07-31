b1a30fca4b37eb20b3987e917e9bd59d5d600005
Alex Korotkov <115859651+Fallet666@users.noreply.github.com>
2024-07-31 18:23:22 +0300

    corrections (log message, log level etc)

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -1,22 +1,22 @@
 #include "logger/logger.h"
 
-#include <utility>
+namespace Logger {
+    Logger::Logger(std::string name, std::ostream &out) : name{std::move(name)}, out{out} {
+    }
 
-Logger::Logger::Logger(std::string name, std::ostream &out) : name{std::move(name)}, out{out} {
-}
+    std::string Logger::formatLog(LogLevel level, const std::string &message) {
+        std::string level_str = toString(level),
+                current_time = fmt::format(
+                    "{:%H:%M:%S}",
+                    fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())));
 
-std::string Logger::Logger::formatLog(LogLevel level, const std::string &message) {
-    std::string level_str = toString(level),
-            current_time = fmt::format(
-                "{:%H:%M:%S}",
-                fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())));
+        /*уровень: Системное время [имя логгера]: message*/
+        std::string formatted_message = fmt::format("{}: {} [{}]: {}\n", level_str, current_time, this->name, message);
+        return formatted_message;
+    }
 
-    /*уровень: Системное время [имя логгера]: message*/
-    std::string formatted_message = fmt::format("{}: {} [{}]: {}", level_str, current_time, this->name, message);
-    return formatted_message;
-}
-
-void Logger::Logger::logMessage(LogLevel level, const std::string &message) {
-    std::lock_guard lock(this->log_mutex);
-    this->out << this->formatLog(level, message) << "\n";
+    void Logger::logMessage(LogLevel level, const std::string &message) {
+        std::lock_guard lock(this->log_mutex);
+        this->out << this->formatLog(level, message);
+    }
 }
