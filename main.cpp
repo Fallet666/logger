4f38e8aaf4459a7f6571b26598cdf9d0713efbc7
Alex Korotkov <115859651+Fallet666@users.noreply.github.com>
2024-08-14 20:58:47 +0300

    color correction

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 int main() {
     Logger::Logger log("abobis");
-    log.logMessage(Logger::DEBUG, "I'm not here");
-    log.logMessage(Logger::INFO, "I'm here");
+    log.logMessage(Logger::DEBUG, "I'm DEBUG");
+    log.logMessage(Logger::INFO, "I'm INFO");
+    log.logMessage(Logger::WARN, "I'm WARNING");
+    log.logMessage(Logger::ERROR, "I'm ERROR");
 }
